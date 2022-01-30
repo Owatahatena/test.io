8061de5852fc3e90eafc12a00d01c5971021a0cd
owatahatena <owatahatena@gmail.com>
2022-01-30 09:48:16 +0900

    'first_version'

--- sample.js
+++ sample.js
@@ -4,3 +4,24 @@ function functionA() {
 }
 
 
+function csv_data(dataPath) {
+    console.log(dataPath);
+    const request = new XMLHttpRequest(); // HTTPでファイルを読み込む
+    request.addEventListener('load', (event) => { // ロードさせ実行
+        const response = event.target.responseText; // 受け取ったテキストを返す
+        csv_array(response); //csv_arrayの関数を実行
+    });
+    request.open('GET', dataPath, true); // csvのパスを指定
+    request.send();
+}
+
+function csv_array(data) {
+    const dataArray = []; //配列を用意
+    const dataString = data.split('\n'); //改行で分割
+    for (let i = 0; i < dataString.length; i++) { //あるだけループ
+        dataArray[i] = dataString[i].split(',');
+    }
+    output_csv.innerHTML = dataArray; //表示
+    console.log(dataArray);
+
+}
